6aa8a8e92524cf840c84934ebbbf98cdc8888274
ishibashi <hogehoge@example.com>
2016-12-26 18:33:35 +0900

    研究室Labsに絡む追加

--- ELab/db/migrate/20161226084105_create_colleges.rb
+++ ELab/db/migrate/20161226084105_create_colleges.rb
@@ -1,7 +1,7 @@
 class CreateColleges < ActiveRecord::Migration[5.0]
   def change
     create_table :colleges do |t|
-      t.column :name, :string, null: false
+      t.column :name, :string, null: false, comment: "大学名"
       t.column :address, :string	
 
       t.timestamps
