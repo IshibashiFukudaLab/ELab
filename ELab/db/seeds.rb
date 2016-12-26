de6ca76a5d1c815d392ab00c6d3e20550e8e6a09
yusuke <yuuuuuuusuke1215@gmail.com>
2016-12-27 02:30:13 +0900

    seed更新

--- ELab/db/seeds.rb
+++ ELab/db/seeds.rb
@@ -8,22 +8,22 @@
 
 College.create(name: "九州大学", address: "福岡県福岡市")
 
-Undergraduate.create(name: "工学部", major: "電気情報工学科")
-Undergraduate.create(name: "農学部")
+# Undergraduate.create(name: "工学部", major: "電気情報工学科")
+# Undergraduate.create(name: "農学部")
 
-Major.create(name: "生物資源環境学科")
+# Major.create(name: "生物資源環境学科")
 
-Field.create(name: "農学分野")
-Field.create(name: "生物生産環境工学分野")
-Field.create(name: "生物生産システム工学分野")
-Field.create(name: "農政経済学分野")
-Field.create(name: "応用生命科学分野")
-Field.create(name: "食糧化学工学分野")
-Field.create(name: "森林機能制御学分野")
-Field.create(name: "森林機能開発学分野")
-Field.create(name: "生物材料機能学分野")
-Field.create(name: "水産化学分野")
-Field.create(name: "アニマルサイエンス分野")
+# Field.create(name: "農学分野")
+# Field.create(name: "生物生産環境工学分野")
+# Field.create(name: "生物生産システム工学分野")
+# Field.create(name: "農政経済学分野")
+# Field.create(name: "応用生命科学分野")
+# Field.create(name: "食糧化学工学分野")
+# Field.create(name: "森林機能制御学分野")
+# Field.create(name: "森林機能開発学分野")
+# Field.create(name: "生物材料機能学分野")
+# Field.create(name: "水産化学分野")
+# Field.create(name: "アニマルサイエンス分野")
 
 Lab.create(name: "植物育種学", mail: "")
 Lab.create(name: "作物学", mail: "")
@@ -88,32 +88,29 @@ Lab.create(name: "畜産化学", mail: "")
 Lab.create(name: "代謝・行動制御額", mail: "")
 
 
-Major.create(name: "電気情報工学科"）
-Major.create(name: "物質科学工学科"）
-Major.create(name: "地球環境工学科"）
-Major.create(name: "エネルギー科学科")
-Major.create(name: "機会航空工学科"）
-Major.create(name: "建築学科"）
+# Major.create(name: "電気情報工学科"）
+# Major.create(name: "物質科学工学科"）
+# Major.create(name: "地球環境工学科"）
+# Major.create(name: "エネルギー科学科")
+# Major.create(name: "機会航空工学科"）
+# Major.create(name: "建築学科"）
 
-Field.create(name: "電気情報学")
-Field.create(name: "化学プロセス・生命工学コース")
-Field.create(name: "応用化学コース")
-Field.create(name: "材料科学工学コース")
-Field.create(name: "建設都市工学コース")
-Field.create(name: "船舶海洋システム工学コース")
-Field.create(name: "地球システム工学コース")
-Field.create(name: "エネルギー科学")
-Field.create(name: "機械工学コース")
-Field.create(name: "航空宇宙工学コース")
-Field.create(name: "建築学")
-
-
+# Field.create(name: "電気情報学")
+# Field.create(name: "化学プロセス・生命工学コース")
+# Field.create(name: "応用化学コース")
+# Field.create(name: "材料科学工学コース")
+# Field.create(name: "建設都市工学コース")
+# Field.create(name: "船舶海洋システム工学コース")
+# Field.create(name: "地球システム工学コース")
+# Field.create(name: "エネルギー科学")
+# Field.create(name: "機械工学コース")
+# Field.create(name: "航空宇宙工学コース")
+# Field.create(name: "建築学")
 
 
 
 
-Lab.create(name: "福田研", mail: "ishibashi@f.ait.kyushu-u.ac.jp", teacher: 1, message: "来てください",theme: "組込み")
-Person.create(lab_id: 1, name: "石橋", grade: "M1")
+
 
 Lab.create(name: "福田研", mail: "ishibashi@f.ait.kyushu-u.ac.jp", teacher: 1, message: "来てください",theme: "組込み")
 Person.create(lab_id: 1, name: "石橋", grade: "M1")
@@ -123,3 +120,62 @@ Person.create(lab_id: 1, name: "石橋", grade: "M1")
 
 Lab.create(name: "福田研", mail: "ishibashi@f.ait.kyushu-u.ac.jp", teacher: 1, message: "来てください",theme: "組込み")
 Person.create(lab_id: 1, name: "石橋", grade: "M1")
+
+Lab.create(name: "福田研", mail: "ishibashi@f.ait.kyushu-u.ac.jp", teacher: 1, message: "来てください",theme: "組込み")
+Person.create(lab_id: 1, name: "石橋", grade: "M1")
+
+
+Lesson.create(college_id: 1, name: "計算機プログラミング演習Ⅰ")
+Lesson.create(college_id: 1, name: "常微分方程式")
+Lesson.create(college_id: 1, name: "応用確率論")
+Lesson.create(college_id: 1, name: "電気情報工学科実験")
+Lesson.create(college_id: 1, name: "回路理論Ⅲ")
+Lesson.create(college_id: 1, name: "システム開発演習")
+Lesson.create(college_id: 1, name: "電子物性")
+Lesson.create(college_id: 1, name: "データ構造とアルゴリズムⅠ演習")
+Lesson.create(college_id: 1, name: "信号とシステム")
+Lesson.create(college_id: 1, name: "エネルギー基礎論")
+Lesson.create(college_id: 1, name: "ディジタル電子回路")
+Lesson.create(college_id: 1, name: "電磁気学Ⅰ")
+Lesson.create(college_id: 1, name: "形式言語とオートマトン")
+Lesson.create(college_id: 1, name: "コンピュータアーキテクチャ")
+Lesson.create(college_id: 1, name: "制御工学Ⅰ")
+Lesson.create(college_id: 1, name: "力学")
+Lesson.create(college_id: 1, name: "オペレーティングシステム")
+Lesson.create(college_id: 1, name: "電磁気学Ⅱ")
+Lesson.create(college_id: 1, name: "電磁気工学Ⅱ")
+Lesson.create(college_id: 1, name: "コンパイラ")
+Lesson.create(college_id: 1, name: "超電導基礎論")
+Lesson.create(college_id: 1, name: "データ解析と実験計画法")
+Lesson.create(college_id: 1, name: "エネルギー変換機器工学")
+Lesson.create(college_id: 1, name: "通信方式")
+Lesson.create(college_id: 1, name: "プラズマ工学")
+Lesson.create(college_id: 1, name: "コンピュータシステムⅡ")
+Lesson.create(college_id: 1, name: "コンピュータシステム通論")
+Lesson.create(college_id: 1, name: "情報ネットワーク")
+Lesson.create(college_id: 1, name: "数理計画法")
+Lesson.create(college_id: 1, name: "電子デバイスⅡ")
+Lesson.create(college_id: 1, name: "電気電子工学設計")
+Lesson.create(college_id: 1, name: "数理論理学")
+
+Lesson.create(college_id: 1, name: "計測工学Ⅱ")
+Lesson.create(college_id: 1, name: "通信ネットワーク")
+Lesson.create(college_id: 1, name: "高電圧・パルスパワー工学")
+Lesson.create(college_id: 1, name: "システムプログラミング演習")
+Lesson.create(college_id: 1, name: "通信工学通論")
+Lesson.create(college_id: 1, name: "アナログ電子回路Ⅱ")
+Lesson.create(college_id: 1, name: "技術表現法")
+Lesson.create(college_id: 1, name: "システム工学")
+Lesson.create(college_id: 1, name: "人工知能")
+Lesson.create(college_id: 1, name: "電気エネルギー工学通論")
+Lesson.create(college_id: 1, name: "パワーエレクトロニクス")
+Lesson.create(college_id: 1, name: "制御工学")
+Lesson.create(college_id: 1, name: "形式言語とオートマトン")
+Lesson.create(college_id: 1, name: "コンピュータアーキテクチャ")
+Lesson.create(college_id: 1, name: "制御工学Ⅰ")
+Lesson.create(college_id: 1, name: "集積回路工学")
+Lesson.create(college_id: 1, name: "アルゴリズム論")
+
+Company.create(name: "??")
+
+LessonLab.create(lesson_id: 1, lab_id: 1)
