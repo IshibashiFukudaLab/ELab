203633f8591b4ba64ca6ab216dc45a43b4c06396
古賀祥造 <koga@f.ait.kyushu-u.ac.jp>
2016-12-27 07:12:14 +0900

    サイドバーに分野を表示するようにした

--- ELab/app/controllers/application_controller.rb
+++ ELab/app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def big_category
+    @big_category = BigCategory.all
+  end
+
+  def get_small_category_by_id(big_id)
+    # @small_category = SmallCategory.find(:all, :conditions => { :big_category_id => big_id })
+    @small_category = SmallCategory.where(big_category_id: big_id)
+
+  end
+
+
+  helper_method :big_category, :get_small_category_by_id
 end
