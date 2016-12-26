5f0ef36fd01dbd34ad7df711a75360f080b452a9
古賀祥造 <koga@f.ait.kyushu-u.ac.jp>
2016-12-26 19:59:34 +0900

    scss環境を構築

--- ELab/db/schema.rb
+++ ELab/db/schema.rb
@@ -12,14 +12,14 @@
 
 ActiveRecord::Schema.define(version: 20161226093143) do
 
-  create_table "colleges", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "colleges", force: :cascade do |t|
     t.string   "name",       null: false
     t.string   "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "labs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "labs", force: :cascade do |t|
     t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
