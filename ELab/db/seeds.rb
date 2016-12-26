8e7ba99ab0ec92b32f91a5bc9f22608268da9f2c
yusuke <yuuuuuuusuke1215@gmail.com>
2016-12-27 00:09:05 +0900

    dev

--- ELab/db/seeds.rb
+++ ELab/db/seeds.rb
@@ -8,5 +8,28 @@
 
 College.create(name: "九州大学", address: "福岡県福岡市")
 
+Undergraduate.create(name: "文学部", major: "電気情報工学科")
+Undergraduate.create(name: "教育学部", major: "電気情報工学科")
+Undergraduate.create(name: "法学部", major: "電気情報工学科")
+Undergraduate.create(name: "経済学部", major: "電気情報工学科")
+Undergraduate.create(name: "理学部", major: "電気情報工学科")
+Undergraduate.create(name: "医学部", major: "電気情報工学科")
+Undergraduate.create(name: "歯学部", major: "電気情報工学科")
+Undergraduate.create(name: "薬学部", major: "電気情報工学科")
+Undergraduate.create(name: "工学部", major: "電気情報工学科")
+Undergraduate.create(name: "芸術学部", major: "電気情報工学科")
+Undergraduate.create(name: "農学部", major: "電気情報工学科")
+Undergraduate.create(name: "21世紀プログラム", major: "電気情報工学科")
+
+
+Lab.create(name: "福田研", mail: "ishibashi@f.ait.kyushu-u.ac.jp", teacher: 1, message: "来てください",theme: "組込み")
+Person.create(lab_id: 1, name: "石橋", grade: "M1")
+
+Lab.create(name: "福田研", mail: "ishibashi@f.ait.kyushu-u.ac.jp", teacher: 1, message: "来てください",theme: "組込み")
+Person.create(lab_id: 1, name: "石橋", grade: "M1")
+
+Lab.create(name: "福田研", mail: "ishibashi@f.ait.kyushu-u.ac.jp", teacher: 1, message: "来てください",theme: "組込み")
+Person.create(lab_id: 1, name: "石橋", grade: "M1")
+
 Lab.create(name: "福田研", mail: "ishibashi@f.ait.kyushu-u.ac.jp", teacher: 1, message: "来てください",theme: "組込み")
 Person.create(lab_id: 1, name: "石橋", grade: "M1")
